3df6983a1f07453f9af72f3c701096fdb9f3ccdb
deovaliandro <mdv16h@student.unhas.ac.id>
2021-05-22 19:27:56 +0800

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Enkripsi-Dekripsi dengan XOR
+
+Untuk enkripsi menggunakan xor pada file file terdapat dua bagian, berupa file sumber yang dalam hal ini adalah msg dan file tempat hasil enkripsi diletakkan (cipher) yaitu file cipher.
+    
+by @deovaliandro
---
+++ xor-dec.cpp
@@ -0,0 +1,33 @@
+/*
+    xor-dec for decription
+*/
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    FILE *fin, *fout;
+    char p, c;
+    std::string key;
+    int i = 0;
+
+    fin = fopen(argv[1], "rb");
+    fout = fopen(argv[2], "wb");
+    
+    if(fin == NULL || fout == NULL){
+        std::cout << "File not found" << std::endl;
+        exit(0);
+    }
+    
+    std::cin >> key;
+
+    while(!feof(fin)){
+        p = getc(fin);
+        c = p ^ key[i];
+        putc(c, fout);
+        i = i + 1 %key.length();
+    }
+
+    fcloseall();
+}
